fa6d188c3e998d88126ce083d3ffd80204ffc48a
parroit <andrea.parodi@ebansoftware.net>
2013-10-12 19:58:05 +0200

    parsing of message

--- test/message-parser_test.js
+++ test/message-parser_test.js
@@ -22,6 +22,89 @@ describe('messageParser',function(){
             expect(messageParser).not.to.be.equal(null);
             expect(messageParser).to.be.a('object');
 
-        })
+        });
+    });
+
+    describe("messageParser",function() {
+        it("should throw on non regexp option argument",function(){
+            (function () {
+                messageParser.run(null,'','');
+            }).should.throw(Error);
+
+            (function () {
+                messageParser.run(undefined,'','');
+            }).should.throw(Error);
+
+            (function () {
+                messageParser.run(true,'','');
+            }).should.throw(Error);
+
+            (function () {
+                messageParser.run({},'','');
+            }).should.throw(Error);
+
+            (function () {
+                messageParser.run(42,'','');
+            }).should.throw(Error);
+
+            (function () {
+                messageParser.run('some options','','');
+            }).should.throw(Error);
+
+            (function () {
+                messageParser.run(function(){},'','');
+            }).should.throw(Error);
+
+        });
+
+
+        it("should throw on non string event name",function(){
+            (function () {
+                messageParser.run(null,'','');
+            }).should.throw(Error);
+
+            (function () {
+                messageParser.run(undefined,'','');
+            }).should.throw(Error);
+
+            (function () {
+                messageParser.run(true,'','');
+            }).should.throw(Error);
+
+            (function () {
+                messageParser.run(/ /,'','');
+            }).should.throw(Error);
+
+            (function () {
+                messageParser.run(42,'','');
+            }).should.throw(Error);
+
+            (function () {
+                messageParser.run({},'','');
+            }).should.throw(Error);
+
+            (function () {
+                messageParser.run(function(){},'','');
+            }).should.throw(Error);
+
+        });
+        it("should listen to stream messages emitted ala fetchmail",function(done){
+            messageParser.run('messageFetched',/.*/);
+
+
+            var bus = require("corriera");
+            var fs = require("fs");
+
+            bus.once('messageParsed', /.*/, function(messageObject){
+                expect(messageObject.from[0].address).to.be.equal("imaptest73@gmail.com");
+
+                done();
+            });
+
+            var messageStream = fs.createReadStream("./test/expected.msg");
+            bus.emit('messageFetched',"any",messageStream);
+
+
+        });
     });
 });
