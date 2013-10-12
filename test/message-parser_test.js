81885792fa20387b439749fba402c2ce24a8a6b5
parroit <andrea.parodi@ebansoftware.net>
2013-10-12 21:22:51 +0200

    parsing of attachments

--- test/message-parser_test.js
+++ test/message-parser_test.js
@@ -88,20 +88,95 @@ describe('messageParser',function(){
             }).should.throw(Error);
 
         });
-        it("should listen to stream messages emitted ala fetchmail",function(done){
-            messageParser.run('messageFetched',/.*/);
+
+        var fs = require("fs");
+
+        var messageStream = fs.createReadStream("./test/expected.msg");
+        var attachments = fs.createReadStream("./test/attachments.msg");
+
+        it("should listen to stream messages emitted ala fetchmail - and emit parsed objects",function(done){
+            messageParser.run('messageFetched',/(.*)/);
 
 
             var bus = require("corriera");
-            var fs = require("fs");
 
-            bus.once('messageParsed', /.*/, function(messageObject){
+            bus.once('messageParsed', /(.*)/, function(messageObject){
                 expect(messageObject.from[0].address).to.be.equal("imaptest73@gmail.com");
 
                 done();
             });
 
-            var messageStream = fs.createReadStream("./test/expected.msg");
+
+            bus.emit('messageFetched',"any",messageStream);
+
+
+        });
+
+        it("should emit streams for attachments",function(done){
+            messageParser.run('messageFetched',/(.*)/);
+
+
+            var bus = require("corriera");
+
+            bus.once('attachmentParsing', /(.*)/, function(attachment,generatedFileName,length,checksum){
+                expect(generatedFileName).to.be.equal("test.txt");
+                //expect(length).to.be.equal(14);
+                //expect(checksum).to.be.equal("");
+
+                var concat = require('concat-stream')
+                var fs = require('fs')
+
+                var write = concat(function(data) {
+                    expect(data.toString('utf8')).to.be.equal("this is a test");
+                    done();
+                });
+
+                attachment.pipe(write);
+
+            });
+
+
+            bus.emit('messageFetched',"any",attachments);
+
+
+        });
+
+
+       /* it("should allow to unlisten messages",function(done){
+            messageParser.run('messageFetched',/(.*)/);
+            messageParser.stop();
+
+
+            var bus = require("corriera");
+
+            bus.once('messageParsed', /(.*)/, function(messageObject){
+                expect(messageObject).to.be.null;
+            });
+
+            setTimeout(function(){
+                done();
+            },1000);
+
+
+
+            bus.emit('messageFetched',"any",messageStream);
+
+
+        });*/
+
+        it("should listen to stream messages emitted ala fetchmail",function(done){
+            messageParser.run('messageFetched',/(.*)/);
+
+
+            var bus = require("corriera");
+
+            bus.once('messageParsed', /(.*)/, function(messageObject){
+                expect(messageObject.from[0].address).to.be.equal("imaptest73@gmail.com");
+
+                done();
+            });
+
+
             bus.emit('messageFetched',"any",messageStream);
 
 
